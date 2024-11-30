0183c6749785411d7f7825efd062a31b1b288bfc
LongWalkAhead <bluelama1234@gmail.com>
2024-11-30 12:55:14 -0500

    new rep

---
+++ test.py
@@ -0,0 +1,6 @@
+import random
+mood= ["happy", "sad", "content", "anxious", "angry", "depressed", "hopeful"]
+while mood == range(8):
+    mood_day = random.choice(mood) 
+    print(mood)
+    print("hello!")
