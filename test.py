d2136fc6e2a7ad44ca0b8bd42fcdbbc717928224
LongWalkAhead <bluelama1234@gmail.com>
2024-12-04 20:34:27 -0500

    new one

--- test.py
+++
@@ -1,6 +0,0 @@
-import random
-moods = ["happy","sad", "energetic","lonely","hopeful"]
-days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday","sunday"]
-for index in range(len(days)):
-    mood = random.choice(moods)
-    print(f"on {days[index]},I am feeling {mood}")
